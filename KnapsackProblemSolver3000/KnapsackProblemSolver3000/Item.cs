58e99b08984bab3e55600035bd62027b67c457dc
BigUngaBunga <truls.rockstrom.ystad.nu@gmail.com>
2022-12-14 12:20:23 +0100

    Implemented simple items
    They have values, weights and comparative values

--- KnapsackProblemSolver3000/KnapsackProblemSolver3000/Item.cs
+++ KnapsackProblemSolver3000/KnapsackProblemSolver3000/Item.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace KnapsackProblemSolver3000
 {
-    public class Item
+    public class Item: IComparable
     {
+        private float weight, value, comparativeValue;
+        public float Weight => weight;
+        public float Value => value;
+        public float ComparativeValue => comparativeValue;
 
+        public Item (float weight, float value)
+        {
+            this.weight = weight;
+            this.value = value;
+            comparativeValue = value / weight;
+        }
+
+        public string ToString() => $"Value {Value}, Weight {Weight}, Comparative Value {ComparativeValue}";
+
+        public int CompareTo(object? obj)
+        {
+            if (obj == null) return 1;
+            if ((obj is Item item))
+            {
+                if (item.ComparativeValue > ComparativeValue)
+                    return 1;
+                else if (item.ComparativeValue < ComparativeValue)
+                    return -1;
+            }
+            return 0;
+        }
     }
 }
--- KnapsackProblemSolver3000/KnapsackProblemSolver3000/Program.cs
+++ KnapsackProblemSolver3000/KnapsackProblemSolver3000/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using KnapsackProblemSolver3000;
+using System.Runtime.CompilerServices;
+
+Random random = new Random();
+List<Item> items = new List<Item>();
+int itemsToFillWith = 250;
+float maxWeight = 15f;
+float maxValue = 21f;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("This just runs?");
+Console.WriteLine("Where's the main method?");
+Console.WriteLine("What the hell is going on?");
+CreateItems();
+PrintItems();
+
+void CreateItems()
+{
+	for (int i = 0; i < itemsToFillWith; i++)
+		items.Add(GetRandomItem());
+    items.Sort();
+}
+
+void PrintItems()
+{
+    for (int i = 0; i < items.Count; i++)
+        Console.WriteLine("Item number " + i + " " + items[i].ToString());
+}
+
+Item GetRandomItem() => new Item((float)random.NextDouble() * maxWeight, (float)random.NextDouble() * maxValue);
